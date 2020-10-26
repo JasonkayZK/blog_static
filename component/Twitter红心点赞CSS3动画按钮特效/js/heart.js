d8f7c1b5ce36a5f5a791eaa8b51f0bebc18baa7d
jasonkayzk <271226192@qq.com>
2020-10-26 21:15:45 +0800

    add twitter like

---
+++ component/Twitter红心点赞CSS3动画按钮特效/js/heart.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $(".heart").on("click", function () {
+
+        var heart = $(this);
+        var likeCount = $("#likeCount" + heart.attr("id").split("like")[1]);
+        var intCount = parseInt(likeCount.html());
+        var rel = heart.attr("rel");
+
+        heart.css("background-position", "");
+
+        if (rel === "like") {
+            likeCount.html(intCount + 1);
+            heart.addClass("heartAnimation").attr("rel", "unlike");
+        } else {
+            likeCount.html(intCount - 1);
+            heart.removeClass("heartAnimation").attr("rel", "like");
+            heart.css("background-position", "left");
+        }
+    }).on("mouseleave", function () {
+        $(this).css("background-position", "");
+    });
+
+});
